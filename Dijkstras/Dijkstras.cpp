534175592d3b8645093eb09e92d65feca785c47c
CHAVEZ RIVAS <45377464+jesse0099@users.noreply.github.com>
2021-10-19 21:49:38 -0600

    AdjacencyLis Dijkstra implementation - More than one edge from start to ends supported

--- Dijkstras/Dijkstras.cpp
+++ Dijkstras/Dijkstras.cpp
@@ -16,6 +16,7 @@ using namespace std;
 /*
 	nv = non valid
 	nvk = non valid key
+	wt = weight - tag
 	uv = unvisited
 	nd = non directed
 	idx = index
@@ -23,14 +24,17 @@ using namespace std;
 */
 #pragma endregion
 
-typedef unordered_map <string, vector<pair<string, int>>> Hash_Graph;
 typedef pair<int, int> int_pair;
+typedef pair<int, string> wt_pair;
+typedef unordered_map <string, vector<wt_pair>> Hash_Graph;
 typedef priority_queue <int_pair, vector<int_pair>, greater<int_pair>> min_heap_pair_queue;
+typedef priority_queue <wt_pair, vector<wt_pair>, greater<wt_pair>> min_heap_wt_pair_queue;
 
 const string NV_INDEX = "No se han indicado vertices validos";
 const string NVK = "No se han indicado llaves validas";
 const unsigned short int INFINITE = 9999;
 const short int NV_VERTEX = -1;
+const string NANKEY = "NAN";
 
 
 #pragma region Vertices
@@ -39,6 +43,8 @@ class Vertex {
 private:
 	unsigned short int index;
 	vector<vector<int>> shortest_paths;
+	//key = Row, pair = (Weight, Pre Vertex)
+	unordered_map<string, wt_pair> hash_shortest_paths;
 	string tag;
 
 public:
@@ -62,6 +68,22 @@ public:
 		return path;
 	}
 
+	vector<wt_pair> hash_shortest_path(string end_vertex) {
+		vector<wt_pair> path;
+		string objective = end_vertex, previous;
+		previous = objective;
+		while (true) {
+			previous = hash_shortest_paths.at(objective).second;
+			if (previous == NANKEY)
+				break;
+			path.insert(path.begin(), make_pair(hash_shortest_paths.at(previous).first,previous));
+			objective = previous;
+		}
+		path.insert(path.begin(), make_pair(hash_shortest_paths.at(end_vertex).first, end_vertex));
+		path.push_back(make_pair(hash_shortest_paths.at(end_vertex).first, end_vertex));
+		return path;
+	}
+
 
 	Vertex() {}
 	Vertex(string p_tag) {
@@ -74,6 +96,9 @@ public:
 	vector<vector<int>> get_shortest_paths() {
 		return shortest_paths;
 	}
+	unordered_map<string, wt_pair> get_hash_shortest_paths() {
+		return hash_shortest_paths;
+	}
 	int get_index(){
 		return index;
 	}
@@ -85,6 +110,9 @@ public:
 	}
 	void set_shortest_path(vector<vector<int>> p_nd_shortest_path) {
 		shortest_paths = p_nd_shortest_path;
+	}
+	void set_hash_shortest_path(unordered_map<string, wt_pair> p_hash_shortest_paths) {
+		hash_shortest_paths = p_hash_shortest_paths;
 	}
 };
 
@@ -109,8 +137,12 @@ private:
 
 	}
 	void init_map_graph() {
-		for (auto x : vertexes_list)
+		size_t i = 0;
+		for (auto x : vertexes_list) {
 			hash_graph[x.get_tag()];
+			vertexes_list[i].set_index(i);
+			i++;
+		}
 	}
 	vector<vector<int>> init_sd_matrix(int vertex_idx) {
 		int org_idx = vertex_idx;//Graph's Matrix row index
@@ -125,6 +157,13 @@ private:
 		}
 
 		return info_table;
+	}
+	unordered_map<string, wt_pair> init_sd_hash(string vertex_tag) {
+		unordered_map<string, wt_pair> returned;
+			for (size_t i = 0; i < vertexes_list.size(); i++)
+				vertex_tag != vertexes_list[i].get_tag() ? returned[vertexes_list[i].get_tag()] = make_pair(INFINITE,"NAN")
+				: returned[vertexes_list[i].get_tag()] = make_pair(0, "NAN");
+		return returned;
 	}
 
 public:
@@ -173,8 +212,8 @@ public:
 	void add_hash_edge(string vertex_a, string vertex_b, int weight, bool bidir = false) {
 		try
 		{
-			bidir ? (hash_graph[vertex_a].push_back(make_pair(vertex_b, weight)), hash_graph[vertex_b].push_back(make_pair(vertex_a, weight)))
-				: hash_graph[vertex_a].push_back(make_pair(vertex_b, weight));;
+			bidir ? (hash_graph.at(vertex_a).push_back(make_pair(weight,vertex_b)), hash_graph.at(vertex_b).push_back(make_pair(weight,vertex_a )))
+				: hash_graph.at(vertex_a).push_back(make_pair(weight,vertex_b));;
 		}
 		catch (const exception&)
 		{
@@ -182,7 +221,7 @@ public:
 		}
 	}
 
-	//Dijkstra shortest path. From vertex org to every other one (Vertex Tag)
+	//Dijkstra shortest path. From vertex org to every other one (Vertex Tag)(Matrix)
 	void shortest_paths(string vertex, int vertex_idx = -1) {
 		//Check if vertex_index returned something != -1
 		int start_vertex = (vertex_idx != -1) ? vertex_idx : vertex_index(vertex);
@@ -202,8 +241,8 @@ public:
 			//Loading priority queue
 			m_q.push(make_pair(0,start_vertex));
 
-			//Visiting all vertexes
-			while (!m_q.empty()) {
+			//Visiting all vertexes 
+			while (!m_q.empty()) { 
 				
 				current_vertex = m_q.top().second;
 
@@ -228,6 +267,63 @@ public:
 		cout << NV_INDEX << endl;
 	}
 	
+	// 	Dijkstra shortest path. From vertex org to every other one (Vertex Tag)(Hash)
+	void hash_shortest_paths(string vertex, int vertex_idx = -1) {
+		//Check if vertex tag exists
+		int org_idx = (vertex_idx != -1) ? vertex_idx : vertex_index(vertex);
+		if(org_idx != -1)
+		{
+			string current_vertex = vertex;
+			vector<wt_pair> current_neighbors;
+			auto info_table = init_sd_hash(vertex);
+			min_heap_wt_pair_queue m_q;
+
+			//Loading priority queue
+			m_q.push(make_pair(0, current_vertex));
+
+			//Visiting all vertexes (Check priority queue size)
+			while (!m_q.empty()) {
+				current_vertex = m_q.top().second;
+				current_neighbors = hash_graph.at(current_vertex);
+				m_q.pop();
+				update_hash_sd(current_vertex,current_neighbors,info_table,m_q);
+			}
+
+			vertexes_list[org_idx].set_hash_shortest_path(info_table);
+			return;
+		}
+
+		cout << NVK << endl;
+	}
+
+	//The shortest path from the start vertex to end vertex (Hash)
+	vector<wt_pair> hash_shortest_path(string start_vertex, string end_vertex) {
+		vector<wt_pair> error_return;
+		int start_vertex_idx = vertex_index(start_vertex), end_vertex_idx;
+		if (start_vertex_idx != -1) {
+			end_vertex_idx = vertex_index(end_vertex);
+			if (end_vertex_idx != -1) {
+				if (vertexes_list[start_vertex_idx].get_shortest_paths().size() > 0) {
+					//Read Table
+					return vertexes_list[start_vertex_idx].hash_shortest_path(end_vertex);
+				}
+				else {
+					//Load Table
+					hash_shortest_paths(start_vertex, start_vertex_idx);
+
+					//Read Table
+					return vertexes_list[start_vertex_idx].hash_shortest_path(end_vertex);
+				}
+			}
+			else {
+				return error_return;
+			}
+		}
+		else {
+			return error_return;
+		}
+	}
+
 	//The shortest path from the start vertex to end vertex
 	vector<int> shortest_path(string start_vertex, string end_vertex) {
 		int start_vertex_idx = vertex_index(start_vertex), end_vertex_idx;
@@ -267,6 +363,19 @@ public:
 				m_q.push(make_pair(info_table[x][0], x));
 			}
 			idx++;
+		}
+	}
+
+	//Update hash infotable
+	void update_hash_sd(string current_vertex, vector<wt_pair>  current_neighbors, unordered_map<string,wt_pair> &info_table, min_heap_wt_pair_queue &m_q) {
+		int start_to_current_distance = info_table.at(current_vertex).first;
+		for (auto x : current_neighbors) {
+			if ((x.first + start_to_current_distance) < info_table.at(x.second).first) {
+				info_table.at(x.second).first = x.first + start_to_current_distance;
+				info_table.at(x.second).second = current_vertex;
+				m_q.push(make_pair(info_table.at(x.second).first,x.second));
+			}
+
 		}
 	}
 
@@ -362,16 +471,29 @@ int main()
 
 
 #pragma region Adjacency List
-	//graph = Graph(vertexes, true);
-	//graph.add_hash_edge("A", "B", 6, true);
-	//graph.add_hash_edge("A", "D", 1, true);
-	//graph.add_hash_edge("B", "D", 2, true);
-	//graph.add_hash_edge("B", "E", 2, true);
-	//graph.add_hash_edge("C", "B", 5, true);
-	//graph.add_hash_edge("C", "E", 5, true);
-	//graph.add_hash_edge("D", "E", 1, true);
+	graph = Graph(vertexes, true);
+	graph.add_hash_edge("A", "B", 6, true);
+	graph.add_hash_edge("A", "D", 1, true);
+	graph.add_hash_edge("B", "E", 2, true);
+	graph.add_hash_edge("B", "D", 1);
+	graph.add_hash_edge("C", "B", 5, true);
+	graph.add_hash_edge("C", "E", 5, true);
+	graph.add_hash_edge("D", "E", 1, true);
+	graph.add_hash_edge("D", "B", 3);
+	graph.add_hash_edge("D", "B", 4);
 
-	//graph.print_hash_graph();
+	vector<wt_pair> path = graph.hash_shortest_path("C", "A");
+	int idx = 0;
+	for (auto x : path) {
+		idx == 0 ? cout << "Shortest Distance " << x.first << endl : (idx == path.size() - 1) ? cout << x.second <<"(" << x.first << ")" << endl
+			: cout << x.second <<"(" << x.first<<")" << "->";
+		idx++;
+	}
+	cout << endl;
+	
+	graph.print_hash_graph();
+
+
 
 #pragma endregion
 
@@ -379,27 +501,28 @@ int main()
 
 
 #pragma region Adjacency matrix
-	graph = Graph(vertexes);
-	graph.add_edge("A", "B", 6, true);
-	graph.add_edge("A", "D", 1, true);
-	graph.add_edge("B", "D", 2);
-	graph.add_edge("B", "E", 2, true);
-	graph.add_edge("C", "B", 5, true);
-	graph.add_edge("C", "E", 5, true);
-	graph.add_edge("D", "E", 1, true);
+	//graph = Graph(vertexes);
+	//graph.add_edge("A", "B", 6, true);
+	//graph.add_edge("A", "D", 1, true);
+	//graph.add_edge("B", "E", 2, true);
+	//graph.add_edge("C", "B", 5, true);
+	//graph.add_edge("C", "E", 5, true);
+	//graph.add_edge("D", "E", 1, true);
+	//graph.add_edge("D", "B", 2);
 	//graph.add_edge("D", "B", 1);
 
 
-	vector<int> path = graph.shortest_path("B", "A");
-	int idx = 0;
-	for (auto x : path) {
-		idx == 0 ? cout << "Shortest Distance " << x << endl : (idx == path.size() - 1) ? cout << graph.get_vertexes_list()[x].get_tag() << endl
-			: cout << graph.get_vertexes_list()[x].get_tag() << "->";
-		idx++;
-	}
-	cout << endl;
 
-	graph.print_graph();
+	//vector<int> path = graph.shortest_path("A", "B");
+	//int idx = 0;
+	//for (auto x : path) {
+	//	idx == 0 ? cout << "Shortest Distance " << x << endl : (idx == path.size() - 1) ? cout << graph.get_vertexes_list()[x].get_tag() << endl
+	//		: cout << graph.get_vertexes_list()[x].get_tag() << "->";
+	//	idx++;
+	//}
+	//cout << endl;
+
+	//graph.print_graph();
 #pragma endregion
 
 
